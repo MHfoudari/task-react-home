2a29b95d68576b10130b75677a6561adc6565879
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-05-28 20:33:22 +0300

    Did the task without map function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import image from "./image_1322693248.webp";
+import products from "./products";
 function App() {
+  const productlist = products.map((product) => {});
   return (
     <>
       <h1 className="stylecauseIcan"> Pharmacy </h1>
-      <p className="stylecauseIcan">
+      <h2 className="stylecauseIcan">
         This is the best pharmacy you'll ever gonna see
-      </p>
-      <img src={image} className="imageclass" />
+      </h2>
+
+      <img src={image} className="imageclass" height="350px" />
+      <h1 className="textstyle">{products[0].name} </h1>
+      <h1 className="textstyle">{products[0].price}</h1>
+      <img src={products[0].pic} className="imageclass" height="300px" />
+      <h1 className="textstyle">{products[1].name} </h1>
+      <h1 className="textstyle">{products[1].price}</h1>
+      <img src={products[1].pic} className="imageclass" />
+
+      {productlist}
     </>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    name: "Panadol",
+    price: "3 KD",
+    pic: "https://cdn.taw9eel.com/media/catalog/product/cache/1/image/519x/9df78eab33525d08d6e5fb8d27136e95/p/a/panadol_advance_48_tablets.jpg",
+  },
+  {
+    name: "ibuprofen",
+    price: "5 KD",
+    pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaSt_uDtzQeWMghHBnmeL5KtfEOVRwyx495Qs2SlNYnEu_Mz4L9x-N0t7l-YvX3TCMfu4&usqp=CAU",
+  },
+];
+
+export default products;
