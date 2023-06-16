7150515c27dd263d39872b6502e47fb5e834b116
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-06-17 01:10:16 +0300

    fixed the listing of products using map method instead of hard-coding it

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import "./App.css";
 import image from "./image_1322693248.webp";
 import products from "./products";
 function App() {
-  const productlist = products.map((product) => {});
+  const productlist = products.map((product) => {
+    return (
+      <div className="textstyle">
+        <img src={product.pic}></img>
+        <h1 className="imageclass">{product.name}</h1>
+        <h1 className="imageclass">{product.price}</h1>
+      </div>
+    );
+  });
   return (
     <>
       <h1 className="stylecauseIcan"> Pharmacy </h1>
       <h2 className="stylecauseIcan">
         This is the best pharmacy you'll ever gonna see
       </h2>
-
       <img src={image} className="imageclass" height="350px" />
-      <h1 className="textstyle">{products[0].name} </h1>
-      <h1 className="textstyle">{products[0].price}</h1>
-      <img src={products[0].pic} className="imageclass" height="300px" />
-      <h1 className="textstyle">{products[1].name} </h1>
-      <h1 className="textstyle">{products[1].price}</h1>
-      <img src={products[1].pic} className="imageclass" />
+      <div class="adjust-line-height">
+        ________________________________________________________________
+      </div>
 
       {productlist}
     </>
@@ -24,3 +28,12 @@ function App() {
 }
 
 export default App;
+//hard-coded code
+{
+  /* <h1 className="textstyle">{products[0].name} </h1>
+<h1 className="textstyle">{products[0].price}</h1>
+<img src={products[0].pic} className="imageclass" height="300px" />
+<h1 className="textstyle">{products[1].name} </h1>
+<h1 className="textstyle">{products[1].price}</h1>
+<img src={products[1].pic} className="imageclass" /> */
+}
